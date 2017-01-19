ff76e49706ad4b4d6968475fc6b3123acbc36cb8
smmitchell2 <smmitchell3@gmail.com>
2017-01-19 12:44:18 -0600

    start of the quiz skill

---
+++ CSQuizIndex.js
@@ -0,0 +1,18 @@
+// Created by Shawn Mitchell January 19, 2017
+
+'use' strict';
+
+var questions = [
+    {
+        "What is a linked list?": [
+            "A structure that connects nodes to each other to form a list",
+            "A structure that stores data based on its value",
+            "A structure that connects multiple nodes to each other"
+        ]
+    },
+    {
+        "What is a max heap": [
+            "A tree like structure that stores the greatest value at the head"
+        ]
+    }
+    ]
